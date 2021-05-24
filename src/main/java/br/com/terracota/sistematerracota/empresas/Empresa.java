453828322eb91f7f044b0c0952828c3cb94c2d7a
alison-mota <alisonalvesmota@gmail.com>
2021-05-24 11:27:57 -0300

    Alison | Criando a API: Correção de IDs duplicados (usei AUTO no lugar de IDENTITY.

--- src/main/java/br/com/terracota/sistematerracota/empresas/Empresa.java
+++ src/main/java/br/com/terracota/sistematerracota/empresas/Empresa.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 public class Empresa {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(updatable = false)
     private Long id;
     @NotBlank
--- src/main/java/br/com/terracota/sistematerracota/fornecedores/Fornecedor.java
+++ src/main/java/br/com/terracota/sistematerracota/fornecedores/Fornecedor.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class Fornecedor {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(updatable = false)
     private Long id;
     @NotBlank
